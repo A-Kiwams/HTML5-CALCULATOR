f44fcdbf167d717e54ad9bca31d74a87cfb33023
NobleKiwam <alhassankiwamdeen@gmail.com>
2019-10-12 09:34:16 -0700

    initial commit

---
+++ file.js
@@ -0,0 +1,35 @@
+// Logic for the calculator 
+
+// declare variable box to hold numbers
+let box = document.getElementById('display');
+
+// create  a function to display no on text box upon clicking on a button
+function addtoscreen(x){
+box.value +=x;
+   if (x == 'c')
+   {
+     box.value = '';
+   }
+}
+
+// create a function to display answer
+function answer(){
+    x = box.value;
+    x = eval(x);
+    box.value = x;
+}
+
+// function for backspace
+function backspace(){
+    let number = box.value;
+    let len = number.length-1;
+    let newnumber = number.substring(0, len);
+    box.value = newnumber;
+}
+
+// function for power
+function power(y){
+    x = box.value;
+    x = Math.pow(x, y);
+    box.value = x;
+}
